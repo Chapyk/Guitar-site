6a9a5495562b3736ea072af72b10e66fdc8e9cdc
Chapy <chapy.kruk@gmail.com>
2026-05-08 20:38:07 +0300

    оновив текст акордів..

--- data/songs.js
+++ data/songs.js
@@ -5,7 +5,85 @@ let songs = [
         artist: "Alex G",
         type: "chords, tabs",
 
-        chords: "Em C G D\nEm C G D",
+        chords: `C Em9 G6
+
+        [Intro]
+
+   C                          Em9              G6
+My dog just ran away and I am feeling pretty bad
+           C                            Em9               G6
+I've been sneaking out at night and my parents are real mad
+          C                                 Em9               G6
+I've been taking lots of drugs because they teach me not to care
+        C                                 Em9                 G6
+Yeah I guess that's how you cheat because life doesn't play fair
+ 
+ 
+[Interlude]
+C Em9 G6 Em9 G6
+Ooooooooh
+ 
+ 
+[Verse 2]
+
+   C                            Em9            G6
+My brother told that he's gonna kill himself tonight
+       C                            Em9           G6
+With a whole bottle of Prozac or a shiny kitchen knife
+    C                                     Em9       G6
+He said that when he's dead I'll have his Nintendo 64
+          C                              Em9              G6
+And I can play it all night long sitting on the basement floor
+ 
+
+[Interlude]
+
+C Em9 G6 Em9 G6
+Ooooooooh
+ 
+ 
+[Verse 3]
+
+    C                             Em9       G6
+My girlfriend told me she doesn't love me anymore
+      C                                Em9              G6
+And I wish I didn't care but I thought she was really cool
+     C                            Em9               G6
+So I drank a ton of liquor then I threw up in her sink
+         C                                 Em9            G6
+She said next time use the toilet then she offered me a drink
+ 
+
+[Interlude]
+
+C Em9 G6 Em9 G6
+Ooooooooh
+ 
+ 
+[Verse 4]
+
+   C                                  Em9         G6
+We passed an old man bleeding on the side of the street
+    C                          Em9               G6
+So I got out of the car and I helped him on his feet
+   C                                    Em9          G6
+His breath smelled like whiskey He was crying like a man
+   C                                           Em9             G6
+When I helped him stand up straight I got his blood all on my hands
+     C                               Em9         G6
+He said "I need a ride home because I got into a fight"
+    C                                          Em9          G6
+Said "I should've seen the other guy but he didn't see my point"
+    C                                     Em9           G6
+He said his house was all the way on the other side of town
+       C                                  Em9          G6
+So we left him in the road and said we wouldn't turn around
+ 
+ 
+[Outro]
+
+C Em9 G6 x3
+`,
 
         tabs: "e|----------------|\nB|----------------|\nG|----------------|"
     },
@@ -16,8 +94,77 @@ let songs = [
         artist: "Alex G",
         type: "chords, tabs",
 
-        chords: "Am F C G",
+        chords: `D E G Baug C Em Am F
 
+
+        [Verse 1]
+
+Am          C          G
+Thinking of things to do
+      F         Em       D
+Yeah, on ly the cheapest things
+     C
+left there for you
+        Am          C               G
+And the only thing I  learned from you
+    F           Em         D
+Is that there's no  thing left
+         C
+to look forward to and
+Am        C         G
+I   was asleep for days
+    F              Em      D
+And now you're the on  ly thing
+        C
+keeping me awake
+    Am      C        G
+The calculator will make
+    F        Em
+the same mistakes
+      D         C
+Yeah, I  see it in its face
+
+
+        [Chorus 1]
+
+D       E          G             Baug
+Hold on tight to this time, this place
+        C             G
+'Cause everything you know
+     C   G
+will be erased
+D         E      G        Baug
+You were born inside your head
+    C                    G
+And that is where you'll be
+     Em       G
+when you are dead
+D             E        G      Baug
+You're just a boy, you are no man
+    C          G         C    G
+And nobody you know will understand
+D             E        G      Baug
+You're just a boy, you are no man
+    C          G         C    G
+And nobody you know will understand
+Em G Em G Em G
+
+
+        [Outro 1]
+Am        C         G
+Welcoming you back home
+    F     Em        D        C
+The on ly one that you have ev er known
+Am        C         G
+Welcoming you back home
+    F     Em        D        C
+The on ly one that you will ev er know
+Am C G F
+Em D C
+Am C G F
+Em D C
+
+`,
         tabs: "e|----------------|\nB|----------------|\nG|----------------|"
     },
 
@@ -27,7 +174,66 @@ let songs = [
         artist: "Валентин Стрикало",
         type: "chords, tabs",
 
-        chords: "Am C G F",
+       chords: ` Dm C A# A
+
+
+               [Verse 1]
+
+Dm          C            A#      A              Dm C A# A
+Так гріє Кохана я вже не хворію Тож завтра я зможу піти
+  Dm       C           A#  A         Dm C A# A
+Подалі Зустрінемося на вокзалі І поїдем туди
+  Dm            C          A#             A
+Де ти намалюєш квіти навколо слідів від куль на стіні
+  Dm            C            A#         A
+Послухай я хочу ділити з тобою кожну хвилину в житті
+  Dm        C          A#    A
+Про те що я завжди чув твій голос
+Dm      C      A#  A
+Ти не дізнаєшся ніколи
+
+ 
+               [Chorus 1]
+
+A                       Dm C A#
+Гаразд тікай а я шукатиму нову
+A                          Dm C A#
+Весь час нехай не гріє я переживу
+A     Dm C A# A
+я переживу
+ 
+
+               [Verse 2]
+
+Dm        C             A#      A             Dm C A# A
+Так темно Послухай хіба я даремно Підпилював ґрати весь рік
+  Dm        C           A#  A        Dm C A# A
+Крізь вікна Я піду непомітно Не відчуваючи ніг
+  Dm            C           A#           A
+Та ти не пробачиш Богові жорстокість його дітей
+  Dm          C         A#         A
+І поїзд поїде без нас тож я не маю інших ідей
+ Dm         C           A#    A
+Окрім як чекати і я сам не свій від болю
+Dm      C      A# A
+Ти не дізнаєшся ніколи
+ 
+
+               [Chorus 2]
+
+A                       Dm C A#
+Гаразд тікай а я шукатиму нову
+A                          Dm C A#
+Весь час нехай не гріє я переживу
+Dm Gm
+ 
+A                       Dm C A#
+Гаразд тікай а я шукатиму нову
+A                          Dm C A#
+Весь час нехай не гріє я переживу
+A     Dm C A# A
+я переживу
+`,
 
         tabs: "e|----------------|\nB|----------------|\nG|----------------|"
     }
