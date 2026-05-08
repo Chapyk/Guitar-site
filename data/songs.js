e81afd90c5f1a5c618de0f89179a442ac2dd6402
Chapy <chapy.kruk@gmail.com>
2026-05-08 14:14:56 +0300

    додав акорди і таби до пісень

--- data/songs.js
+++ data/songs.js
@@ -1,11 +1,13 @@
-// songs data
-
 let songs = [
     {
         id: 1,
         name: "Nintendo 64",
         artist: "Alex G",
-        type: "chords"
+        type: "chords",
+
+        chords: "Em C G D\nEm C G D",
+
+        tabs: "e|----------------|\nB|----------------|\nG|----------------|"
     },
 
     {
@@ -13,12 +15,20 @@ let songs = [
         name: "Things to do",
         artist: "Alex G",
         type: "tabs",
+
+        chords: "Am F C G",
+
+        tabs: "e|----------------|\nB|----------------|\nG|----------------|"
     },
 
-      {
+    {
         id: 3,
         name: "Так гріє",
         artist: "Валентин Стрикало",
         type: "chords",
-    },
+
+        chords: "Am C G F",
+
+        tabs: "e|----------------|\nB|----------------|\nG|----------------|"
+    }
 ]
