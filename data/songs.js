27fff974b6bc8a738442102d538186892dd0685b
Chapy <chapy.kruk@gmail.com>
2026-05-08 13:36:45 +0300

    додав масив пісень

---
+++ data/songs.js
@@ -0,0 +1,24 @@
+// songs data
+
+let songs = [
+    {
+        id: 1,
+        name: "Numb",
+        artist: "Linkin Park",
+        type: "chords"
+    },
+
+    {
+        id: 2,
+        name: "Boulevard of Broken Dreams",
+        artist: "Green Day",
+        type: "tabs"
+    },
+
+    {
+        id: 3,
+        name: "Smells Like Teen Spirit",
+        artist: "Nirvana",
+        type: "chords"
+    }
+]
